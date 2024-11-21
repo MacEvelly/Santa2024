2cd9fb85834ac434c7e83300caccebef97021c98
Brett Williams <MacEvelly@gmail.com>
2024-11-21 00:29:22 +0000

    imported Santa

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,1 +1,2 @@
-export { default as Logo } from './svg/logo.svg'
+export { default as Logo } from "./svg/logo.svg";
+export { default as Santa } from "./glb/Santa.glb";
--- src/components/3D/Scenes/Scene3D.jsx
+++ src/components/3D/Scenes/Scene3D.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { Box, Stage } from "../";
 import { Plane } from "@react-three/drei";
+import { Player } from "../elements/Character";
 
 export function Scene3D() {
   return (
     <Stage>
       <Box position={[-1.2, 1, 0]} castShadow receiveShadow />
       <Box position={[1.2, 1, 0]} castShadow receiveShadow />
-      <Plane position={[0, 0.05, 0]} args={[10, 10]} rotation={[-Math.PI / 2,0,0]} receiveShadow castShadow >
+      <Plane
+        position={[0, 0.05, 0]}
+        args={[10, 10]}
+        rotation={[-Math.PI / 2, 0, 0]}
+        receiveShadow
+        castShadow
+      >
         <meshStandardMaterial />
       </Plane>
+      <Player />
     </Stage>
   );
 }
